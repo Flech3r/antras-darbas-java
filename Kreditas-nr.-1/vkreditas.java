f29b126725606d3d4ec6d886e2bed4c8768d5d38
Flech3r <sangrealwow1@gmail.com>
2015-12-16 21:07:13 +0200

    Kodo sutvarkymas

--- Kreditas-nr.-1/vkreditas.java
+++ Kreditas-nr.-1/vkreditas.java
@@ -11,23 +11,22 @@ public class vkreditas {
 	}
 	
 	public void kreditas(){
-		
 		double Vimk, Ksum; //papildomi kintamieji, taip pat palukanu norma
-		
 		if(is<15000)
 			if (k>=300&&k<=260000)
-				if(l<=5)
-				{		Ksum=kreditosuma(k,l);
-						Vimk=imokosAp(Ksum, l);
-						if(((Vimk/a)*100)<=40)
-							System.out.println("Kreditą suteikti galima!" + " "
-									+ "Viso gražinti reikės: " + Ksum + " " 
-									+ "Menėsine įmoka bus: " + Vimk);
-							else System.out.println("Kredito suteikti negalime! ");}
-						else System.out.println("Kredito suteikti negalime! Perilgas terminas!");
-				else System.out.println("Kredito suteikti negalime! Prašoma suma yra permaža arba per didelė!");
-		else System.out.println("Kredito suteikti negalime! Jusu įsiskolinimai per dideli");	
-		
+				if(l<=5){		
+					Ksum=kreditosuma(k,l);
+					Vimk=imokosAp(Ksum, l);
+					if(((Vimk/a)*100)<=40)
+						System.out.println("Kreditą suteikti galima!" + " " + "Viso gražinti reikės: " + Ksum + " " + "Menėsine įmoka bus: " + Vimk);
+					else 
+						System.out.println("Kredito suteikti negalime! ");
+					}else 
+						System.out.println("Kredito suteikti negalime! Perilgas terminas!");
+					else 
+						System.out.println("Kredito suteikti negalime! Prašoma suma yra permaža arba per didelė!");
+					else 
+						System.out.println("Kredito suteikti negalime! Jusu įsiskolinimai per dideli");	
 	}
 	private double kreditosuma(double k, double l){
 		double p=0.04; //metine palukanu norma 4%
