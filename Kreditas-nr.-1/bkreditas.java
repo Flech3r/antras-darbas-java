0b6dbc6d41215315246c4160fa1d995553cf49e6
Flech3r <sangrealwow1@gmail.com>
2015-12-16 21:08:52 +0200

    Kodo sutvarkymas

--- Kreditas-nr.-1/bkreditas.java
+++ Kreditas-nr.-1/bkreditas.java
@@ -12,27 +12,23 @@ private double a, k, l, is, pa;
 	}
 	
 	public void kreditas(){
-		
-		double Vimk, Ksum; //papildomi kintamieji
-		
+		double Vimk, Ksum;
 		if(is<5000)
-				if(pa<=50)
-				{		Ksum=kreditosuma(k,l);
-						Vimk=imokosAp(Ksum, l);
-						if(((Vimk/a)*100)<=40)
-							System.out.println("Kreditą suteikti galima!" + " "
-									+ "Viso gražinti reikės: " + Ksum + " " 
-									+ "Menėsinė įmoka bus: " + Vimk);
-							else System.out.println("Kredito suteikti negalime! ");}
-						else System.out.println("Kredito suteikti negalime! Pastatas Persenas!");
-		else System.out.println("Kredito suteikti negalime! Jūsų įsiskolinimai per dideli");	
-		
+			if(pa<=50)
+				{
+				Ksum=kreditosuma(k,l);
+				Vimk=imokosAp(Ksum, l);
+				if(((Vimk/a)*100)<=40)
+					System.out.println("Kreditą suteikti galima!" + " " + "Viso gražinti reikės: " + Ksum + " " + "Menėsinė įmoka bus: " + Vimk);
+				else System.out.println("Kredito suteikti negalime! ");
+				}else System.out.println("Kredito suteikti negalime! Pastatas Persenas!");
+				else System.out.println("Kredito suteikti negalime! Jūsų įsiskolinimai per dideli");	
 	}
 	private double kreditosuma(double k, double l){
-		double p=0.04; //metine palukanu norma 4%
+		double p=0.04;
 		return k+(k*l*p);
 	}
 	private double imokosAp(double K, double l){
-		return k/(l*12); // imokos apskaiciavimas
+		return k/(l*12);
 	}
 }
